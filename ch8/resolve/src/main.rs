7099866bf14e4e05c8198c4a6763e6f7a310bdd4
zelr0x <zelr0xff@gmail.com>
2022-06-22 03:14:51 +0400

    #1 - update trust-dns dependency

--- ch8/resolve/src/main.rs
+++ ch8/resolve/src/main.rs
@@ -3,15 +3,19 @@ use std::time::Duration;
 
 use clap::{App, Arg};
 use rand;
-use trust_dns::op::{Message, MessageType, OpCode, Query};
-use trust_dns::rr::domain::Name;
-use trust_dns::rr::record_type::RecordType;
-use trust_dns::serialize::binary::*;
+use trust_dns_client::op::{Message, MessageType, OpCode, Query};
+use trust_dns_client::rr::domain::Name;
+use trust_dns_client::rr::record_type::RecordType;
+use trust_dns_client::serialize::binary::*;
 
 fn main() {
     let app = App::new("resolve")
         .about("A simple to use DNS resolver")
-        .arg(Arg::with_name("dns-server").short("s").default_value("1.1.1.1"))
+        .arg(
+            Arg::with_name("dns-server")
+                .short("s")
+                .default_value("1.1.1.1"),
+        )
         .arg(Arg::with_name("domain-name").required(true))
         .get_matches();
     let domain_name = app.value_of("domain-name").unwrap();
@@ -37,20 +41,18 @@ fn main() {
     let timeout = Duration::from_secs(3);
     localhost.set_read_timeout(Some(timeout)).unwrap();
     localhost.set_nonblocking(false).unwrap();
-    let _amt = localhost.send_to(&req_b, dns_server)
+    let _amt = localhost
+        .send_to(&req_b, dns_server)
         .expect("socket misconfigured");
-    let (_amt, _remote) = localhost.recv_from(&mut resp_b)
-        .expect("timeout_reached");
-    let dns_message = Message::from_vec(&resp_b)
-        .expect("unable to parse response");
+    let (_amt, _remote) = localhost.recv_from(&mut resp_b).expect("timeout_reached");
+    let dns_message = Message::from_vec(&resp_b).expect("unable to parse response");
 
     for ans in dns_message.answers() {
-        if ans.record_type() == RecordType::A {
-            let resource = ans.rdata();
-            let ip = resource.to_ip_addr()
-                .expect("invalid IP address received");
+        if ans.rr_type() == RecordType::A {
+            // TODO: improve error message
+            let resource = ans.data().expect("No resource data in received record");
+            let ip = resource.to_ip_addr().expect("invalid IP address received");
             println!("{}", ip.to_string());
         }
     }
 }
-
