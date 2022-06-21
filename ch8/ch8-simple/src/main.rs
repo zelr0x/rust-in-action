fb23156ea776203db8e7a1796cf6c4fbcba5c372
zelr0x <zelr0xff@gmail.com>
2022-06-22 01:57:02 +0400

    #1 - update dependencies of ch8/ch8-simple

--- ch8/ch8-simple/src/main.rs
+++ ch8/ch8-simple/src/main.rs
@@ -2,12 +2,10 @@ use std::error::Error;
 
 use reqwest;
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     let url = "http://www.rustinaction.com/";
-    let mut response = reqwest::get(url)?;
+    let response = reqwest::blocking::get(url)?;
     let content = response.text()?;
     print!("{}", content);
     Ok(())
 }
-
